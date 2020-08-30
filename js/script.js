4c4e624ca39393fc98b8d015a348ed4de865d9da
Tal <talj.gelfand@gmail.com>
2020-08-30 21:11:05 +0300

    Собираем наш проект и фиксим баги

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'), // собираем кусочки скрипта вместе
-          modal = require('./modules/modal'),
-          timer = require('./modules/timer'),
-          cards = require('./modules/cards'),
-          calc = require('./modules/calc'),
-          forms = require('./modules/forms'),
-          slider = require('./modules/slider');
+import tabs from './modules/tabs'; // собираем кусочки скрипта вместе
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
 
-    tabs();
-    modal();
-    timer();
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000); 
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2020-09-19');
     cards();
     calc();
-    forms();
-    slider();
+    forms('form', modalTimerId);
+    slider({ // используем деструктуризацию объекта (slider.js)
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
     
 });
