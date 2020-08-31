fe43852f795404583a931a2bc9eba1eb9898c800
Tal <talj.gelfand@gmail.com>
2020-08-31 13:12:18 +0300

    Как превратить код ES6+ в старый формат ES5. Babel, Core.js и полифиллы

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs'; // собираем кусочки скрипта вместе
 import modal from './modules/modal';
 import timer from './modules/timer';
